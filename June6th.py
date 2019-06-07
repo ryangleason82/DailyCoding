c1b408c3ab54410210ff976fccd871ba04a8c79a
Ryan Gleason <ryangleason82@gmail.com>
2019-06-07 06:48:53 -0400

    Not good!

---
+++ June6th.py
@@ -0,0 +1,15 @@
+# Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded.
+
+# For example, the message '111' would give 3, since it could be decoded as 'aaa', 'ka', and 'ak'.
+
+# You can assume that the messages are decodable. For example, '001' is not allowed.
+
+
+def numDecode(msg):
+    dict = {}
+    # I know there's a sumple way to map each number to each letter
+    dict = {{1, 26}, {a, z}}
+    return 0
+
+
+numDecode(111)
